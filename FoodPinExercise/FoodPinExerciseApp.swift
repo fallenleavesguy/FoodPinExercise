27ac3f47ea352c04bd2a0d7cc6b840ae3773f474
donghs <no@example.com>
2026-08-07 11:26:18 +0800

    chore: add images

--- FoodPinExercise/FoodPinExerciseApp.swift
+++ FoodPinExercise/FoodPinExerciseApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FoodPinExerciseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RestaurantListView()
         }
     }
 }
--- FoodPinExercise/RestaurantListView.swift
+++ FoodPinExercise/RestaurantListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RestaurantListView.swift
 //  FoodPinExercise
 //
 //  Created by donghs on 8/7/26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RestaurantListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RestaurantListView()
 }
