0e51a7013b242de0f8c1b67a81136bd0f055e680
donghs <no@example.com>
2026-08-07 10:36:49 +0800

    Initial Commit

---
+++ FoodPinExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPinExercise
+//
+//  Created by donghs on 8/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodPinExercise/FoodPinExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinExerciseApp.swift
+//  FoodPinExercise
+//
+//  Created by donghs on 8/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
